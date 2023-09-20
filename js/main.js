c5dfd78f333afaaf092a3fdd379bde00888d587e
Andrew Ortiz <steelpreyer@gmail.com>
2023-09-20 16:45:22 +0000

    Define a function named renderEntry with a single parameter named entry. Create an event listener on the document which listensn for the DOMContentLoaded event. Remove the example HTML entries from index.html. Create a function named toggleNoEntries which toggles the no entries text to show or hide when the function is called. #2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const $imageInput = document.querySelector('#photoUrl');
+const $imageInput = document.querySelector('#photo');
 const $image = document.querySelector('img');
 const $form = document.querySelector('form');
 
@@ -9,7 +9,7 @@ $imageInput.addEventListener('input', function (event) {
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
   const title = $form.elements.title.value;
-  const photoUrl = $form.elements.photoUrl.value;
+  const photoUrl = $form.elements.photo.value;
   const notes = $form.elements.notes.value;
   const inputValues = {
     entryId: data.nextEntryId,
@@ -24,3 +24,50 @@ $form.addEventListener('submit', function (event) {
   $image.setAttribute('src', './images/placeholder-image-square.jpg');
   $form.reset();
 });
+
+function renderEntry(entry) {
+  const $entryList = document.createElement('li');
+  $entryList.className = 'row';
+
+  const $entryDiv = document.createElement('div');
+  $entryDiv.className = 'column-half';
+
+  const $entryImg = document.createElement('img');
+  $entryImg.setAttribute('src', entry.photoUrl);
+  $entryImg.setAttribute('alt', entry.title);
+
+  const $Divtwo = document.createElement('div');
+  $Divtwo.className = 'column-half';
+
+  const $h1Entry = document.createElement('h1');
+  $h1Entry.textContent = entry.title;
+
+  const $pElement = document.createElement('p');
+  $pElement.textContent = entry.notes;
+
+  $entryList.appendChild($entryDiv);
+  $entryDiv.appendChild($entryImg);
+  $Divtwo.appendChild($h1Entry);
+  $Divtwo.appendChild($pElement);
+  $entryList.appendChild($Divtwo);
+
+  return $entryList;
+}
+
+const $ul = document.querySelector('.entry-list');
+document.addEventListener('DOMContentLoaded', function (event) {
+  for (let i = 0; i < data.entries.length; i++) {
+    const $data = renderEntry(data.entries[i]);
+    $ul.appendChild($data);
+  }
+  toggleNoEntries();
+});
+
+const $noEntries = document.querySelector('.no-entries');
+function toggleNoEntries() {
+  if (data.entries.length > 0) {
+    $noEntries.classList.add('hidden');
+  } else {
+    $noEntries.classList.remove('hidden');
+  }
+}
